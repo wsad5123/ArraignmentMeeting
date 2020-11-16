13a359325065aaaae00f8b7bbd7faf116e634c81
wsad5123 <wsad5123@vip.qq.com>
2020-11-16 13:49:19 +0800

    1.完成首页绘制

--- app/src/main/java/com/qiaosong/arraignmentmeeting/ui/activity/family/MainFamilyActivity.java
+++ app/src/main/java/com/qiaosong/arraignmentmeeting/ui/activity/family/MainFamilyActivity.java
@@ -1,23 +1,27 @@
 package com.qiaosong.arraignmentmeeting.ui.activity.family;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.qiaosong.arraignmentmeeting.R;
 import com.qiaosong.arraignmentmeeting.ui.base.BaseActivity;
 import com.qiaosong.arraignmentmeeting.ui.mvp.contacts.MainFamilyContacts;
 import com.qiaosong.arraignmentmeeting.ui.mvp.presenter.MainFamilyPresenter;
 
-import butterknife.BindView;
-
 public class MainFamilyActivity extends BaseActivity<MainFamilyPresenter> implements MainFamilyContacts.IMainFamilyView {
-
-    @BindView(R.id.tv_name)
-    TextView tvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean isShowActionBar() {
+        return false;
+    }
+
+    @Override
+    public boolean isShowStatusTitle() {
+        return false;
     }
 
     @Override
--- app/src/main/java/com/qiaosong/arraignmentmeeting/ui/viewholder/ActionBarMenuHolder.java
+++ app/src/main/java/com/qiaosong/arraignmentmeeting/ui/viewholder/ActionBarMenuHolder.java
@@ -6,8 +6,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.qiaosong.arraignmentmeeting.R;
 import com.qiaosong.arraignmentmeeting.ui.base.BaseViewHolder;
-import com.qiaosong.baselibrary.R;
 import com.qiaosong.baselibrary.callback.OnActionBarMenuClickListener;
 
 
