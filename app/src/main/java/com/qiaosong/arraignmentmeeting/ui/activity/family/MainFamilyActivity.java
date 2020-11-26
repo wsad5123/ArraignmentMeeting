a998468a6af8e67e7923f5ff3ec9408a12e4c2a7
zhangxinyu <123456>
2020-11-26 18:07:27 +0800

    1.新增设置界面

--- app/src/main/java/com/qiaosong/arraignmentmeeting/ui/activity/family/MainFamilyActivity.java
+++ app/src/main/java/com/qiaosong/arraignmentmeeting/ui/activity/family/MainFamilyActivity.java
@@ -3,6 +3,9 @@ package com.qiaosong.arraignmentmeeting.ui.activity.family;
 import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -62,10 +65,13 @@ public class MainFamilyActivity extends BaseActivity<MainFamilyPresenter> implem
     @BindView(R.id.rl_parent)
     RelativeLayout rlParent;
 
+    StringBuffer stringBuffer = new StringBuffer();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         rlParent.addView(new TitleViewHolder(mContext, rlParent).getView());
+        initCode();
     }
 
     @Override
@@ -128,4 +134,101 @@ public class MainFamilyActivity extends BaseActivity<MainFamilyPresenter> implem
             }
         }, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.ACCESS_WIFI_STATE});
     }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_ENTER:     //确定键enter
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+                btnSure.performClick();
+                break;
+            case KeyEvent.KEYCODE_0:
+                inputNum("0");
+                break;
+            case KeyEvent.KEYCODE_1:
+                inputNum("1");
+                break;
+            case KeyEvent.KEYCODE_2:
+                inputNum("2");
+                break;
+            case KeyEvent.KEYCODE_3:
+                inputNum("3");
+                break;
+            case KeyEvent.KEYCODE_4:
+                inputNum("4");
+                break;
+            case KeyEvent.KEYCODE_5:
+                inputNum("5");
+                break;
+            case KeyEvent.KEYCODE_6:
+                inputNum("6");
+                break;
+            case KeyEvent.KEYCODE_7:
+                inputNum("7");
+                break;
+            case KeyEvent.KEYCODE_8:
+                inputNum("8");
+                break;
+            case KeyEvent.KEYCODE_9:
+                inputNum("9");
+                break;
+            case KeyEvent.KEYCODE_DEL:    //删除
+                removeNum();
+                break;
+            case KeyEvent.KEYCODE_BACK:    //返回键
+                break;   //这里由于break会退出，所以我们自己要处理掉 不返回上一层
+            default:
+                break;
+        }
+        return super.onKeyDown(keyCode, event);
+
+    }
+
+    private void initCode() {
+        vFour.setVisibility(View.GONE);
+        vThree.setVisibility(View.GONE);
+        vTwo.setVisibility(View.GONE);
+        vOne.setVisibility(View.GONE);
+        if (stringBuffer.length() == 3) {
+            vFour.setVisibility(View.VISIBLE);
+        } else if (stringBuffer.length() == 2) {
+            vThree.setVisibility(View.VISIBLE);
+        } else if (stringBuffer.length() == 1) {
+            vTwo.setVisibility(View.VISIBLE);
+        } else if (stringBuffer.length() == 0) {
+            vOne.setVisibility(View.VISIBLE);
+        }
+        tvOne.setText("");
+        tvTwo.setText("");
+        tvThree.setText("");
+        tvFour.setText("");
+        for (int i = 0; i < stringBuffer.length(); i++) {
+            if (i == 0) {
+                tvOne.setText(stringBuffer.toString().substring(0, 1));
+            } else if (i == 1) {
+                tvTwo.setText(stringBuffer.toString().substring(1, 2));
+            } else if (i == 2) {
+                tvThree.setText(stringBuffer.toString().substring(2, 3));
+            } else if (i == 3) {
+                tvFour.setText(stringBuffer.toString().substring(3, 4));
+            }
+        }
+    }
+
+
+    public void inputNum(String num) {
+        if (!TextUtils.isEmpty(num) && stringBuffer.length() < 4) {
+            stringBuffer.append(num);
+        }
+        initCode();
+    }
+
+    public void removeNum() {
+        if (stringBuffer.length() > 1) {
+            stringBuffer.deleteCharAt(stringBuffer.length() - 1);
+        }
+        initCode();
+    }
+
 }
