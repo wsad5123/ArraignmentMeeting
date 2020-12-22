dcc006704d7a6641ed85f7d4233bac147cf35475
zhangxinyu <123456>
2020-12-22 17:34:15 +0800

    1.家属首页适配遥控器完成

--- app/src/main/java/com/qiaosong/arraignmentmeeting/ui/activity/family/MainFamilyActivity.java
+++ app/src/main/java/com/qiaosong/arraignmentmeeting/ui/activity/family/MainFamilyActivity.java
@@ -6,13 +6,10 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.hst.fsp.FspEngine;
 import com.qiaosong.arraignmentmeeting.AppApplication;
 import com.qiaosong.arraignmentmeeting.R;
 import com.qiaosong.arraignmentmeeting.bean.LoginTokenBean;
@@ -35,43 +32,22 @@ import org.greenrobot.eventbus.Subscribe;
 
 import butterknife.BindView;
 import butterknife.OnClick;
-import butterknife.OnEditorAction;
-import butterknife.OnTextChanged;
 import io.reactivex.functions.Consumer;
 
 public class MainFamilyActivity extends BaseActivity<MainFamilyPresenter> implements MainFamilyContacts.IMainFamilyView {
+
     @BindView(R.id.tv_one)
     TextView tvOne;
-    @BindView(R.id.v_one)
-    View vOne;
-    @BindView(R.id.rl_one)
-    RelativeLayout rlOne;
     @BindView(R.id.tv_two)
     TextView tvTwo;
-    @BindView(R.id.v_two)
-    View vTwo;
-    @BindView(R.id.rl_two)
-    RelativeLayout rlTwo;
     @BindView(R.id.tv_three)
     TextView tvThree;
-    @BindView(R.id.v_three)
-    View vThree;
-    @BindView(R.id.rl_three)
-    RelativeLayout rlThree;
     @BindView(R.id.tv_four)
     TextView tvFour;
-    @BindView(R.id.v_four)
-    View vFour;
-    @BindView(R.id.rl_four)
-    RelativeLayout rlFour;
     @BindView(R.id.btn_sure)
     Button btnSure;
-    @BindView(R.id.v_setting)
-    View vSetting;
     @BindView(R.id.rl_parent)
     RelativeLayout rlParent;
-    @BindView(R.id.et_code)
-    EditText etCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,85 +75,100 @@ public class MainFamilyActivity extends BaseActivity<MainFamilyPresenter> implem
         return new MainFamilyPresenter(this);
     }
 
-    @OnClick({R.id.btn_sure, R.id.v_setting})
+    @OnClick({R.id.btn_sure, R.id.rl_setting})
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.v_setting:
+            case R.id.rl_setting:
                 startActivity(new Intent(mContext, AdminPasswordActivity.class));
                 break;
             case R.id.btn_sure:
-                if (etCode.getText().length() == 4) {
-                    mvpPresenter.getToken(etCode.getText().toString());
+                if (getCode().length() == 4) {
+                    mvpPresenter.getToken(getCode());
                 } else {
                     ToastUtils.show(mContext, "请输入4位会见码");
                 }
                 break;
-            case R.id.tv_one:
-            case R.id.tv_two:
-            case R.id.tv_three:
-            case R.id.tv_four:
-                InputManagerUtils.showInput(this, etCode);
-                break;
         }
     }
 
-    @OnTextChanged(R.id.et_code)
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-        tvOne.setText("");
-        tvTwo.setText("");
-        tvThree.setText("");
-        tvFour.setText("");
-        vFour.setVisibility(View.GONE);
-        vThree.setVisibility(View.GONE);
-        vTwo.setVisibility(View.GONE);
-        vOne.setVisibility(View.GONE);
-        int length = s.length();
-        switch (length) {
-            case 4:
-                tvFour.setText(s.subSequence(3, 4));
-            case 3:
-                tvThree.setText(s.subSequence(2, 3));
-            case 2:
-                tvTwo.setText(s.subSequence(1, 2));
-            case 1:
-                tvOne.setText(s.subSequence(0, 1));
-                break;
+    private String getCode() {
+        String one = tvOne.getText().toString();
+        String two = tvTwo.getText().toString();
+        String three = tvThree.getText().toString();
+        String four = tvFour.getText().toString();
+        String code = "";
+        if (!TextUtils.isEmpty(one)) {
+            code = one;
         }
-        switch (length) {
-            case 3:
-                vFour.setVisibility(View.VISIBLE);
-                break;
-            case 2:
-                vThree.setVisibility(View.VISIBLE);
-                break;
-            case 1:
-                vTwo.setVisibility(View.VISIBLE);
-                break;
-            case 0:
-                vOne.setVisibility(View.VISIBLE);
-                break;
+        if (!TextUtils.isEmpty(two)) {
+            code += two;
         }
+        if (!TextUtils.isEmpty(three)) {
+            code += three;
+        }
+        if (!TextUtils.isEmpty(four)) {
+            code += four;
+        }
+        return code;
     }
 
-    /**
-     * 搜索按钮触发回调
-     *
-     * @param v
-     * @param actionId
-     * @param event
-     * @return
-     */
-    @OnEditorAction(R.id.et_code)
-    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-            if (etCode.getText().length() == 4) {
-                mvpPresenter.getToken(etCode.getText().toString());
-            } else {
-                ToastUtils.show(mContext, "请输入4位会见码");
-            }
-        }
-        return false;
-    }
+//    @OnTextChanged(R.id.et_code)
+//    public void onTextChanged(CharSequence s, int start, int before, int count) {
+//        tvOne.setText("");
+//        tvTwo.setText("");
+//        tvThree.setText("");
+//        tvFour.setText("");
+////        vFour.setVisibility(View.GONE);
+////        vThree.setVisibility(View.GONE);
+////        vTwo.setVisibility(View.GONE);
+////        vOne.setVisibility(View.GONE);
+//        int length = s.length();
+//        switch (length) {
+//            case 4:
+//                tvFour.setText(s.subSequence(3, 4));
+//            case 3:
+//                tvThree.setText(s.subSequence(2, 3));
+//            case 2:
+//                tvTwo.setText(s.subSequence(1, 2));
+//            case 1:
+//                tvOne.setText(s.subSequence(0, 1));
+//                break;
+//        }
+////        switch (length) {
+////            case 3:
+////                vFour.setVisibility(View.VISIBLE);
+////                break;
+////            case 2:
+////                vThree.setVisibility(View.VISIBLE);
+////                break;
+////            case 1:
+////                vTwo.setVisibility(View.VISIBLE);
+////                break;
+////            case 0:
+////                vOne.setVisibility(View.VISIBLE);
+////                break;
+////        }
+//    }
+
+//    /**
+//     * 搜索按钮触发回调
+//     *
+//     * @param v
+//     * @param actionId
+//     * @param event
+//     * @return
+//     */
+//    @OnEditorAction(R.id.et_code)
+//    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+//            if (etCode.getText().length() == 4) {
+//                mvpPresenter.getToken(etCode.getText().toString());
+//            } else {
+//                ToastUtils.show(mContext, "请输入4位会见码");
+//            }
+//        }
+//        return false;
+//    }
 
     @Subscribe
     public void onEvent(TagValueEvent event) {
@@ -216,7 +207,7 @@ public class MainFamilyActivity extends BaseActivity<MainFamilyPresenter> implem
                 return true;
             case KeyEvent.KEYCODE_ENTER:     //确定键enter
             case KeyEvent.KEYCODE_DPAD_CENTER:
-                btnSure.performClick();
+//                btnSure.performClick();
                 break;
             default:
                 break;
